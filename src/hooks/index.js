266be4de23b70d1a3209beff16a4c6241e44cc03
impeccable548 <igheleraro2@gmail.com>
2025-11-07 20:57:04 +0100

    Create index.js

---
+++ src/hooks/index.js
@@ -0,0 +1,6 @@
+// Export all hooks from a single file for cleaner imports
+export { useProjects } from './useProjects'
+export { useSkills } from './useSkills'
+export { useContact } from './useContact'
+export { useAdmin } from './useAdmin'
+export { useScroll } from './useScroll'
